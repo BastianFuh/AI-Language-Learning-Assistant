5ceb765f8caa05ab72ea59fa82843c13b7ed07f0
Baeshen <bastian.fuhlenriede@gmail.com>
2024-08-14 20:57:43 +0200

    Updated main to test current system

---
+++ src/main.py
@@ -0,0 +1,36 @@
+""" Main module.
+"""
+import logging
+import os
+
+from core.processing import DummyActionProcess
+from audio.sounddevice_recorder import SoundDeviceRecorderModule
+from audio.whisper_speech_recognition import WhisperSpeechRecognitionModule
+
+
+logging.basicConfig(level="DEBUG")
+
+if __name__ == "__main__":
+    if 'DEFAULT_SOUNDDEVICE' in os.environ.keys():
+        soundDevice = SoundDeviceRecorderModule(
+            duration=WhisperSpeechRecognitionModule.DEFAULT_DURATION, device=os.environ['DEFAULT_SOUNDDEVICE'])
+    else:
+        soundDevice = SoundDeviceRecorderModule(
+            duration=WhisperSpeechRecognitionModule.DEFAULT_DURATION)
+
+    speechRecognition = WhisperSpeechRecognitionModule(
+        in_data=soundDevice.audio_buffer,
+        e_din_avail=soundDevice.e_audio_ready
+    )
+
+    processing_1 = DummyActionProcess(
+        speechRecognition.out_data,
+        speechRecognition.e_dout_avail
+    )
+
+    speechRecognition.start()
+    soundDevice.start()
+    processing_1.start()
+
+    while True:
+        pass
